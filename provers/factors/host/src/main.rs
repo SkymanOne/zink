abd51ba28be1b196849d5017879277b0a23bf879
SkymanOne <german@parity.io>
2023-07-17 14:08:23 +0200

    auto proof submission

--- provers/factors/host/src/main.rs
+++ provers/factors/host/src/main.rs
@@ -6,6 +6,85 @@ use risc0_zkvm::{
     ExecutorEnv,
 };
 
+use clap::Parser;
+use codec::{Decode, Encode};
+use sp_core::blake2_256;
+use std::str::FromStr;
+use subxt::{
+	blocks::ExtrinsicEvents,
+	config::WithExtrinsicParams,
+	ext::{
+		sp_core::{
+			sr25519::{Pair as SubxtPair},
+			Pair as SubxtPairT,
+		},
+		sp_runtime::AccountId32,
+	},
+	tx::{BaseExtrinsicParams, PairSigner, PlainTip},
+	Error, OnlineClient, PolkadotConfig, SubstrateConfig,
+};
+// Runtime types, etc
+#[subxt::subxt(runtime_metadata_path = "./metadata.scale")]
+pub mod substrate_node {}
+
+use substrate_node::runtime_types::sp_weights::weight_v2::Weight;
+
+
+async fn send_receipt(
+	contract: AccountId32,
+	receipt: &SessionReceipt,
+) -> Result<
+	ExtrinsicEvents<
+		WithExtrinsicParams<SubstrateConfig, BaseExtrinsicParams<SubstrateConfig, PlainTip>>,
+	>,
+	Error,
+> {
+	let receipt_scale_encoded = to_vec(receipt).unwrap().encode();
+	let receipt_recreated: SessionReceipt =
+		from_slice(&Vec::<u32>::decode(&mut &receipt_scale_encoded[..]).unwrap()).unwrap();
+
+	// If fails, the contract won't be able to verify the proof. If passed, all should be good for the contract to verify it
+	assert_eq!(&receipt_recreated, receipt);
+
+	let mut call_data = Vec::<u8>::new();
+	//append the selector
+	call_data.append(&mut (blake2_256("verify_via_ext".as_bytes())[0..4]).to_vec());
+	//append the arguments
+	call_data.append(&mut to_vec(receipt).unwrap().encode());
+
+	let call_tx = substrate_node::tx().contracts().call(
+		// MultiAddress::Id(contract)
+		contract.into(),
+		0, // value
+		// Both need checking, or values from estimates. These ones come from contracts ui
+		Weight { ref_time: 160_106_502_144, proof_size: 40_898_144 }, // gas_limit
+		None,                                                  // storage_deposit_limit
+		// To zkvm's serialization, then to SCALE encoding
+		call_data,
+	);
+
+	let api = OnlineClient::<PolkadotConfig>::new().await.unwrap();
+
+	// This is just Alice, which is okay for an example
+	let restored_key = SubxtPair::from_string(
+		"0xe5be9a5092b81bca64be81d212e7f2f9eba183bb7a90954f7b76361f6edb5c0a",
+		None,
+	)
+	.unwrap();
+	let signer = PairSigner::new(restored_key);
+
+	let result = api
+		.tx()
+		.sign_and_submit_then_watch_default(&call_tx, &signer)
+		.await?
+		.wait_for_in_block()
+		.await?
+		.fetch_events()
+		.await?;
+
+	Ok(result)
+}
+
 // Multiply them inside the ZKP
 pub fn multiply_factors(a: u64, b: u64) -> (SessionReceipt, u64) {
     let env = ExecutorEnv::builder()
@@ -35,8 +114,36 @@ pub fn multiply_factors(a: u64, b: u64) -> (SessionReceipt, u64) {
     (receipt, c)
 }
 
-fn main() {
-     // Pick two numbers
-    let (receipt, _) = multiply_factors(17, 23);
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+	/// Contract address
+	#[arg(short, long)]
+	contract_address: String,
+}
 
+#[tokio::main]
+async fn main() {
+	let args = Args::parse();
+	// Pick two numbers
+	let (receipt, _) = multiply_factors(17, 23);
+	// println!("Receipt size: {}", to_vec(&receipt).unwrap().len());
+
+	let contract_account = AccountId32::from_str(&args.contract_address).unwrap();
+
+	println!("IMAGE_ID {:?}", MULTIPLY_ID);
+
+	// Verify receipt, panic if it's wrong
+	receipt.verify(MULTIPLY_ID).expect(
+		"Code you have proven should successfully verify; did you specify the correct image ID?",
+	);
+
+	let r_vec = to_vec(&receipt).unwrap();
+	let receipt2: Result<SessionReceipt, _> = from_slice(&r_vec);
+	let receipt2 = receipt2.unwrap();
+
+	assert_eq!(receipt, receipt2);
+	let res = send_receipt(contract_account, &receipt).await;
+	println!("Result: {}", res.is_ok());
 }
